1a2d1e79a1d8713986a1fa3c9de20eab91b661d4
KVerhofstadt <floxsta@gmail.com>
2016-11-30 14:49:09 +0100

    readAnimals functie

--- StartPointForExercises/StartPointForExercises/Assignments/AnimalShelter/product/file_element.c
+++ StartPointForExercises/StartPointForExercises/Assignments/AnimalShelter/product/file_element.c
@@ -4,42 +4,65 @@
 
 int readAnimals(const char* filename, ANIMAL* animalPtr, int nrAnimals)
 {
-	if(filename == NULL || *filename == "" || animalPtr == NULL)
+	//unsigned char buffer[1000];
+	FILE* fp;
+	int animalsInFile = 0;
+	char mode = 'r';
+	if(filename == NULL || animalPtr == NULL)
 	{
 		return -1;
 	}
-	int animalsInFile;
-
-}
-
-int writeAnimals(const char* filename, const ANIMAL* animalPtr, int nrAnimals)
-{
-
-}
-
-int getNrAnimalsInFile(const char* filename)
-{
-	int animalsInFile;
-	if(filename == NULL || *filename == "")
+	fp = fopen(filename, &mode);
+	if(nrAnimals == getNrAnimalsInFile)
+	{
+		animalsInFile = fread(animalPtr, sizeof(ANIMAL), nrAnimals, fp);	
+	}
+	else if(getNrAnimalsInFile < nrAnimals)
+	{
+		animalsInFile = fread(animalPtr, sizeof(ANIMAL), nrAnimals, fp);
+	}
+	else
 	{
 		return -1;
 	}
-	fopen(filename, r);
+	return animalsInFile;
+
+
+}
+
+// int writeAnimals(const char* filename, const ANIMAL* animalPtr, int nrAnimals)
+// {
+
+// }
+
+// int getNrAnimalsInFile(const char* filename)
+// {
+// 	//int animalsInFile;
+// 	unsigned char buffer[1000];
+// 	FILE* ptr;
+// 	if(filename == NULL || *filename == "")
+// 	{
+// 		return -1;
+// 	}
+// 	ptr = fopen(filename, rb);
+// 	fread(buffer, sizeof(buffer), , ptr);
+// 	animalsInFile = sizeof(buffer) / sizeof(ANIMAL);
+
 	
 	
-}
+// }
 
-int readAnimalFromFile(const char* filename, int filePosition, ANIMAL* animalPtr)
-{
+// int readAnimalFromFile(const char* filename, int filePosition, ANIMAL* animalPtr)
+// {
 
-}
+// }
 
-int writeAnimalToFile(const char* filename, int filePosition, const ANIMAL* animalPtr)
-{
+// int writeAnimalToFile(const char* filename, int filePosition, const ANIMAL* animalPtr)
+// {
 
-}
+// }
 
-int renameAnimalInFile(const char* filename, int filePosition, const char* animalSurname)
-{
+// int renameAnimalInFile(const char* filename, int filePosition, const char* animalSurname)
+// {
 
-}
+// }
